72a882b4abaef93b00d1b7b4245236dcd5dedaf7
rishabh-smpx <rishabh.singh@smartprix.com>
2019-08-28 16:00:14 +0530

    Add map component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Map from './components/Map';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="/"
-          rel="noopener noreferrer"
-        >
-          Restaurant Finder
-        </a>
-      </header>
+      <div className="sidebar">
+        <header className="header">
+          <img src={logo} className="logo" alt="logo" />
+          <span className="name">Restaurant Search</span>
+        </header>
+      </div>
+      <Map className="map" />
     </div>
   );
 }
