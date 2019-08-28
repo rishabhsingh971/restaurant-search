5549eda1312ced51681ad4187ffb82d070d30b2c
rishabhsingh971 <rishabh.singh@smartprix.com>
2019-08-29 03:16:57 +0530

    Add restaurant component

---
+++ src/components/Restaurants.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default class Restaurants extends React.Component {
+  render() {
+    const restaurants = this.props.restaurants;
+    console.log(restaurants)
+    if (!restaurants) return null;
+    restaurants.maps((restaurant, i) => {
+      const markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
+      const markerIcon = this.MARKER_PATH + markerLetter + '.png';
+      return (
+        <tr
+          style={{backgroundColor: i % 2 === 0 ? '#F0F0F0' : '#FFFFFF'}}
+          onClick={() => this.props.onClick(i)}
+        >
+          <td><img className="placeIcon" src={markerIcon} alt="place icon" /></td>
+          <td>{restaurant.name}</td>
+        </tr>
+      );
+    })
+    return (
+      <div id="listing">
+        <table id="resultsTable">
+          <tbody id="results">
+            {restaurants}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
