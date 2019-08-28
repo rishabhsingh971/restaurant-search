14b7503a9cc454382a65495148270d3201e93cfc
rishabhsingh971 <rishabh.singh@smartprix.com>
2019-08-29 01:10:19 +0530

    Change App from function to class

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 import GoogleMap from './components/GoogleMap';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="sidebar">
-        <header className="header">
-          <img src={logo} className="logo" alt="logo" />
-          <span className="name">Restaurant Search</span>
-        </header>
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="sidebar">
+          <header className="header">
+            <img src={logo} className="logo" alt="logo" />
+            <span className="name">Restaurant Search</span>
+          </header>
+        </div>
+        <GoogleMap />
       </div>
-      <GoogleMap />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
