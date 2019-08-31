2b31bea8c0b5aa05086a4b8653485ae60a5874bb
rishabhsingh971 <rishabhsingh971@gmail.com>
2019-09-01 00:00:29 +0530

    Updates

---
+++ precache-manifest.101f07f7e42b5e79e99cf9500e813d0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ac3bd3ff825aece287a3d997380cf3",
+    "url": "/restaurant-search/index.html"
+  },
+  {
+    "revision": "c7d0f493d67be978993b",
+    "url": "/restaurant-search/static/css/main.f51a4b67.chunk.css"
+  },
+  {
+    "revision": "f191bbc18ecef774d5fa",
+    "url": "/restaurant-search/static/js/2.e9d4b249.chunk.js"
+  },
+  {
+    "revision": "c7d0f493d67be978993b",
+    "url": "/restaurant-search/static/js/main.3ce4086c.chunk.js"
+  },
+  {
+    "revision": "52910921d629102e256b",
+    "url": "/restaurant-search/static/js/runtime~main.50dc2ebf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/restaurant-search/static/media/logo.5d5d9eef.svg"
+  }
+]);
